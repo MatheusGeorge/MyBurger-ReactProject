7e6b5f8ede3d2a960690e426c75be0f5ba7c1a23
Matheus Vieira <matheus.vs2015@hotmail.com>
2020-04-30 22:17:25 -0300

    Aula 234

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,27 +91,14 @@ class BurguerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice.toFixed(2),
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '12345',
-                    country: 'Germany'
-                },
-            email: 'test@teste.com'
-            },
-            deliveryMethod: 'fatest'
+        const queryParams = [];
+        for(let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-        .then(response => {
-           this.setState({loading: false, purchasing: false});
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing: false});
+        queryParams.push('price=' + this.state.totalPrice);
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryParams.join('&')
         });
     }
 
